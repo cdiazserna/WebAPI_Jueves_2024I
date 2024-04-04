70ad6cece455b1b46ee8986560e9f3fe59acdac4
Carlor Jr Diaz <cjunior90@gmail.com>
2024-04-04 17:21:41 -0500

    Configuración de la BD

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Esta es la línea de code que necesito para configurar la conexión a la BD
+builder.Services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
