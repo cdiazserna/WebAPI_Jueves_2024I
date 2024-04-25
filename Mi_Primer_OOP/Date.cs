6df65d1b1ad6ceb5382a5d2cf9b0b3e046a1ca32
Carlor Jr Diaz <cjunior90@gmail.com>
2024-04-24 21:33:30 -0500

    Mi primer programa POO de Date resuelto

---
+++ Mi_Primer_OOP/Date.cs
@@ -0,0 +1,24 @@
+﻿namespace Mi_Primer_OOP
+{
+    public class Date
+    {
+        //Properties
+        public int Day {  get; set; } //getters and setters
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+        //Methods
+        public Date(int day, int month, int year) //day=5, month= 12, year=2024
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        public string ShowDate()
+        {
+            var date = String.Format("{0}/{1}/{2}", Day, Month, Year);
+            return date;
+        }
+    }
+}
---
+++ Mi_Primer_OOP/Program.cs
@@ -0,0 +1,28 @@
+﻿using Mi_Primer_OOP;
+using System.Drawing;
+using System.Xml.Linq;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declaración de variables
+        int day, month, year;
+
+        //pedir datos por pantalla
+        Console.Write("Ingrese el día: ");
+        day = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ingrese el mes: ");
+        month = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ingrese el año: ");
+        year = Convert.ToInt32(Console.ReadLine());
+
+        //Crear instancia del objeto Date
+        Date date = new Date(day, month, year);
+
+        string myDate = date.ShowDate(); //Esta es la forma de acceder a las propiedas/métodos de una clase
+
+        Console.WriteLine(myDate);
+        Console.WriteLine(date.ShowDate());
+    }
+}
