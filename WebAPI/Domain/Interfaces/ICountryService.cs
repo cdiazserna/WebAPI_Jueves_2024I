8ca42ab971222bcbf480fb9c9d7ec899dae7cb85
Carlor Jr Diaz <cjunior90@gmail.com>
2024-04-25 19:05:19 -0500

    Services layer developed

--- WebAPI/Domain/Interfaces/ICountryService.cs
+++ WebAPI/Domain/Interfaces/ICountryService.cs
@@ -5,15 +5,11 @@ namespace WebAPI.Domain.Interfaces
 {
     public interface ICountryService
     {
-        Task<IEnumerable<Country>> GetCountriesAsync(); //Una de las tantas firmas de un método!
-
-        Task<Country> CreateCountryAsync(Country country);
-
-
+        //Una de las tantas firmas de un método!
+        Task<IEnumerable<Country>> GetCountriesAsync();
         Task<Country> GetCountryByIdAsync(Guid id);
-
+        Task<Country> CreateCountryAsync(Country country);
         Task<Country> EditCountryAsync(Country country);
-
         Task<Country> DeleteCountryAsync(Guid id);
     }
 }
