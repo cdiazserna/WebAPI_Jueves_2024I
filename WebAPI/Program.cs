2eb10c143f21729001c3fb739c97ca6ac46ef162
Carlor Jr Diaz <cjunior90@gmail.com>
2024-05-17 21:57:02 -0500

    CountriesController created

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using WebAPI.DAL;
+using WebAPI.Domain.Interfaces;
+using WebAPI.Domain.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,9 @@ builder.Services.AddControllers();
 
 //Esta es la línea de code que necesito para configurar la conexión a la BD
 builder.Services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Contenedor de Dependencias
+builder.Services.AddScoped<ICountryService, CountryService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
